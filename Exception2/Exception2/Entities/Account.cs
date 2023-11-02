01ea2d6cb04591ca04e8443ff7bc42a1b49d76de
bbraga99 <brunuu.henrique@hotmail.com>
2023-11-02 08:07:59 -0300

    Add exercice 2

---
+++ Exception2/Exception2/Entities/Account.cs
@@ -0,0 +1,53 @@
+﻿using System.Globalization;
+using Exception2.Entities.Exceptions;
+
+namespace Exception2.Entities
+{
+    internal class Account
+    {
+        public int AccountNumber { get; set; }
+        public string Holder { get; set; }
+        public double Balance { get; set; }
+        public double WithdrawLimit { get; set; }
+
+        public Account()
+        {
+        }
+        public Account(int accountNumber, string holder, double initialBalance, double withdrawLimit)
+        {
+            AccountNumber = accountNumber;
+            Holder = holder;
+            Balance += initialBalance;
+            WithdrawLimit = withdrawLimit;
+        }
+
+        public void Deposit(double  amount)
+        {
+            Balance += amount;
+        }
+
+        public void Withdraw(double amount)
+        {
+            if(amount > WithdrawLimit)
+            {
+                throw new AccountException("The amount is bigger than withdraw limit");
+            }
+            if(amount > Balance)
+            {
+                throw new AccountException("Not enought balance");
+            }
+            Balance -= amount;
+        }
+
+        public override string ToString()
+        {
+            return AccountNumber
+                + ", "
+                + Holder
+                + ", "
+                + Balance.ToString("F2", CultureInfo.InvariantCulture)
+                + ", "
+                + WithdrawLimit.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Exception2/Exception2/Program.cs
@@ -0,0 +1,41 @@
+﻿using Exception2.Entities;
+using Exception2.Entities.Exceptions;
+using System.Globalization;
+
+namespace Course
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter account data: ");
+                Console.Write("Number: ");
+                int accountNumber = int.Parse(Console.ReadLine());
+                Console.Write("Holder: ");
+                string holderName = Console.ReadLine();
+                Console.Write("Initial balance: ");
+                double initialBalance = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Withdraw limit: ");
+                double withdrawLimit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                Account account = new Account(accountNumber, holderName, initialBalance, withdrawLimit);
+                Console.WriteLine(account);
+
+                Console.WriteLine();
+
+                Console.Write("Enter amount for withdraw: ");
+                double amount = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                account.Withdraw(amount);
+
+                Console.WriteLine(account);
+            } catch (AccountException e )
+            {
+                Console.WriteLine("Withdraw error: " + e.Message );
+            }
+            
+        }
+    }
+
+}
